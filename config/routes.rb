d49861204c10ef99c34e28e1a5c8bf5e9322a11e
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-31 22:55:28 +0530

    Generated devise & make default home

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
 
-  get 'home/index'
-  
-  get 'home/index'
   root 'home#index'
+
+  resources :home, only: [:index] 
+
 end
