25224970a205ac7cafde02f58222ea2d632dde49
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-31 22:08:47 +0530

    Add devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   get 'home/index'
   
   get 'home/index'
