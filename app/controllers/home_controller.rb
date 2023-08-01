578f24c730d49b7ea1c038fda85a252b39381109
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-08-01 14:09:04 +0300

    Add current user at the controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,8 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    if_signed_in? do |current_user|
+      @user_name = current_user.name
+    end
+    @user_name ||= "Guest"
+  end
 end
