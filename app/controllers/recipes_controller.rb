ed423ac12b062dce3ac59de8833c45921497b5a4
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-08-03 00:38:23 +0300

    Fix the recipe id on controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -66,6 +66,7 @@ class RecipesController < ApplicationController
   private
 
   # Use callbacks to share common setup or constraints between actions.
+  
   def set_recipe
     @recipe = Recipe.find(params[:id])
   end
