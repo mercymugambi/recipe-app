b4ba1785bfc4cbd4764321222347ec511b6ec3bc
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-08-02 23:44:18 +0300

    Update user table with has many

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id_id'
-  has_many :recipe_foods
+  has_many :recipe_foods, foreign_key: 'recipe_id_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
 
-  has_many :foods
-  has_many :recipes
+          has_many :foods, foreign_key: 'user_id_id'
+          has_many :recipes, foreign_key: 'user_id_id'
+        
 end
