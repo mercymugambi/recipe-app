bb5794d0a4963ddb5ed6dd90f5e966aec4fc0134
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-08-03 02:21:24 +0300

    Add the buttons to the recipe

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -3,11 +3,13 @@ class RecipeFoodsController < ApplicationController
   before_action :set_recipe_food, only: [:edit, :update, :destroy]
 
   def new
+    @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = @recipe.recipe_foods.new
     @foods = current_user.foods
   end
 
   def create
+    @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = @recipe.recipe_foods.new(recipe_food_params)
     if @recipe_food.save
       redirect_to @recipe, notice: 'Recipe Food was successfully added.'
