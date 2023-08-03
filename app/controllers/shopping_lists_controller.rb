ce18367956ad49b2ef4cc5a6caeb56cfadae06c3
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-08-03 23:51:15 +0530

    Finished Shoping list

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,14 +1,26 @@
 class ShoppingListsController < ApplicationController
   def index
     @foods = Food.where(user_id_id: current_user.id)
-    @recipes = Recipe.all
+    @recipes = Recipe.where(user_id_id: current_user.id)
     @recipess = []
     @recipes.each do |recipe|
-      recipe.recipe_foods do |recipe_food|
-        @recipess.push(recipe_food.food)
-      end
+      @recipess << recipe.recipe_foods
     end
     def convert(foood)
+      obj_food = foood.first.map do |recipe_food|
+        {
+          id: recipe_food.id,
+          quantity: recipe_food.quantity,
+          recipe_id_id: recipe_food.recipe_id_id,
+          food_id_id: recipe_food.food_id_id,
+          created_at: recipe_food.created_at,
+          updated_at: recipe_food.updated_at,
+          value: recipe_food.value
+        }
+      end
+      return obj_food
+    end
+    def converting(foood)
       obj_food = foood.map do |row|
         {
           id: row.id,
@@ -23,11 +35,13 @@ class ShoppingListsController < ApplicationController
       end
       return obj_food
     end
-    general_food = convert(@foods)
-    reciped_food = convert(@recipess)
-    
-    result = general_food.reject { |obj| reciped_food.map { |o| o[:id] }.include?(obj[:id]) }
+    @newRecipess = convert(@recipess)
+    @genFood = converting(@foods)
+    result = @genFood.reject { |obj| @newRecipess.map { |o| o[:food_id_id] }.include?(obj[:id]) }
 
     @shoppings = result
   end
 end
+
+  #   recipe.recipe_foods do |recipe_food|
+  # end
