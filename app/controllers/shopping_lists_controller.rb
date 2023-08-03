85936c591b8d76390ebee4b6e8a068b9b076b180
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-08-03 15:23:21 +0530

    Fix linter errors

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,8 +1,7 @@
 class ShoppingListsController < ApplicationController
-    def index
-        @foods = Food.where(user_id_id: current_user.id)
-        @recipes = Recipe.foods
-        @shoppings = @foods - @recipes
-
-    end
+  def index
+    @foods = Food.where(user_id_id: current_user.id)
+    @recipes = Recipe.foods
+    @shoppings = @foods - @recipes
+  end
 end
