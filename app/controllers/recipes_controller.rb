149dd4605d0ecc84eeac2727497f85f50b19eee0
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-08-04 16:53:15 +0530

    Fixed n+1 problem from shopping lists

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,7 @@ class RecipesController < ApplicationController
 
   # GET /recipes or /recipes.json
   def index
-    @recipes = Recipe.include(:recipe_foods).all
+    @recipes = Recipe.includes(:recipe_foods, :foods).all
   end
 
   # GET /recipes/1 or /recipes/1.json
