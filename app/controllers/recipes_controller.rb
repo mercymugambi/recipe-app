849a77bbd6db6bc76a7d91fb0e7b28fccffc00c3
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-08-04 13:27:06 +0300

    Remove include food on recipe index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,7 @@ class RecipesController < ApplicationController
 
   # GET /recipes or /recipes.json
   def index
-    @recipes = Recipe.includes(:recipe_foods).all
+    @recipes = Recipe.all
   end
 
   # GET /recipes/1 or /recipes/1.json
